36cc450f8a5b40b46eae8d1b2897cd235a97b578
Salvador Pedro Damaso <84344681+spdDamaso@users.noreply.github.com>
2024-10-29 22:56:09 -0600

    Add files via upload

---
+++ ingles/src/main/java/com/example/ingles/model/Vervos.java
@@ -0,0 +1,41 @@
+package com.example.ingles.model;
+
+import lombok.Data;
+
+
+
+public class Vervos {
+	private String verboEnEspanol;
+	private String infinitivo;
+	private String pasadoSimple;
+	private String participio;
+	public String getVerboEnEspanol() {
+		return verboEnEspanol;
+	}
+	public void setVerboEnEspanol(String vervoEnEspanol) {
+		this.verboEnEspanol = vervoEnEspanol;
+	}
+	public String getInfinitivo() {
+		return infinitivo;
+	}
+	public void setInfinitivo(String infinitivo) {
+		this.infinitivo = infinitivo;
+	}
+	public String getPasadoSimple() {
+		return pasadoSimple;
+	}
+	public void setPasadoSimple(String pasadoSimple) {
+		this.pasadoSimple = pasadoSimple;
+	}
+	public String getParticipio() {
+		return participio;
+	}
+	public void setParticipio(String participio) {
+		this.participio = participio;
+	}
+	
+	
+	
+	
+	
+}
---
+++ ingles/src/test/java/com/example/ingles/InglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InglesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
